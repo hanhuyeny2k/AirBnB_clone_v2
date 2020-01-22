078fdf140b445ea41c7de2bcfdd647d370e5c06d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-22 02:12:15 +0000

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+Listening on 0.0.0.0, port 5000, route /c/<text>
+"""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """ return Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def path_hbnb():
+    """ return HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_is(text):
+    """ return C <text> """
+    return 'C {}'.format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
