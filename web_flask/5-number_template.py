9abc5be9a13c27970ffca5e4a337ab3f50793d58
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-22 03:20:21 +0000

    display html only if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+"""
+Listening on 0.0.0.0, port 5000, route /c/<text>
+"""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """ return Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def path_hbnb():
+    """ return HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_is(text):
+    """ return C <text> """
+    new_text = text.replace("_", " ")
+    return 'C {}'.format(new_text)
+
+
+@app.route('/python/<text>')
+@app.route('/python/')
+@app.route('/python')
+def python_is(text='is cool'):
+    """ return Python is cool by default """
+    if text:
+        new_text = text.replace("_", " ")
+        return 'Python {}'.format(new_text)
+
+
+@app.route('/number/<int:n>')
+def int_only(n):
+    """ display 'n is a number' only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def HTML_only(n=None):
+    """ display HTML page only if n in an integer"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
