c08849ed284f39a64ac3fb6a9b09746301657d70
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-22 00:21:34 +0000

    Added strict_slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,10 @@ Start Flask web app, listening on 0.0.0.0, port 5000, display Hello HBNB!
 """
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """ return Hellow HBNB """
     return "Hello HBNB!"
 app.run(host="0.0.0.0", port=5000)
-
+app.route.map(strict_slashes=False)
