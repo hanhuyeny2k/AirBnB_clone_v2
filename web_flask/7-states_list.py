ef3742c460d7b91454a21b7d296bed13de951e11
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-23 05:06:56 +0000

    fixed pep8 error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,5 +23,6 @@ def HTML_States(states=None):
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
